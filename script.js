13931841bd5f5077ea65af2f46650033c7c6579c
chirag jain <jchirag0610@gmail.com>
2026-06-13 07:14:46 +0530

    add task modal opening work in progress

---
+++ script.js
@@ -0,0 +1,6 @@
+modal = document.querySelector(".modal");
+addBtn = document.querySelector(".column .heading button");
+
+addBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+});
